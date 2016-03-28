763b87f44db4732793623f6cc0f62d79593b700f
garnachod <garnachod@gmail.com>
2016-03-28 19:09:01 +0200

    Definición de GenerateSentimentTrain.py

---
+++ LuigiTasks/GenerateSentimenTrain.py
@@ -0,0 +1,42 @@
+from DBbridge.ConsultasCassandra import ConsultasCassandra
+from Config.Conf import Conf
+import luigi
+
+
+class GenerateTextByLang(luigi.Task):
+	"""
+		GenerateTextByLang
+
+	genera un fichero de texto con los tweets con un sentimiento definido (positivo o negativo)
+	en un lenguaje dado
+	El formato de salida es:
+	0 SENTIMIENTO_identificadorunico
+	1 texto tweet
+
+	0 y 1 son el numero de linea, por lo que las lineas pares contienen la etiqueta
+	y las lineas impares contienen texto.
+
+	El sentimiento puede ser POS, NEG o NAN (si no lo hemos identificado)
+	"""
+	lang = luigi.Parameter()
+
+	def output(self):
+		conf = Conf()
+		path = conf.getAbsPath()
+		return luigi.LocalTarget('%s/Data/%s.train'%(self.lang))
+
+
+	def run(self):
+		"""
+		Ejecucion:
+
+		se puede utilizar getTweetsTextAndLang en ConsultasCassandra,
+		por cada tweet comprobar el sentimiento, si es que tiene.
+		se pueden almacenar tweets en el idioma dado pero que no contengan sentimiento 
+		esto mejora el procesamiento de texto.
+		"""
+
+		"""
+		Por otro lado para procesar el texto, ProcesadoresTexto.LimpiadorTweets contiene todo.
+		"""
+		pass
