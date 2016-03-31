34793df4249d5a4c7af873e71b7f53ee6e3596fc
garnachod <garnachod@gmail.com>
2016-03-31 11:50:25 +0200

    D2v train

--- LuigiTasks/GenerateSentimentModel.py
+++ LuigiTasks/GenerateSentimentModel.py
@@ -1,6 +1,7 @@
 from LuigiTasks.GenerateSentimentTrain import GenerateTextByLang
 from ProcesadoresTexto.Doc2Vec import Doc2Vec
 from Config.Conf import Conf
+import numpy as np
 import luigi
 
 class GenerateNLPByLang(luigi.Task):
@@ -15,7 +16,7 @@ class GenerateNLPByLang(luigi.Task):
 	def output(self):
 		conf = Conf()
 		path = conf.getAbsPath()
-		return luigi.LocalTarget('%s/Data/%s.d2v'%(self.lang))
+		return luigi.LocalTarget('%s/Data/%s.check'%(self.lang))
 
 	def requires(self):
 		return GenerateTextByLang(self.lang)
@@ -24,7 +25,7 @@ class GenerateNLPByLang(luigi.Task):
 		with self.output().open("w") as out:
 			d2v = Doc2Vec()
 			savePath = self.path.replace("check","model")
-			
+
 			conf = Conf()
 			d2v.train(self.input().path, savePath, dimension = conf.getDimVectors(), epochs = 20, method="DBOW")
 			out.write("OK")
@@ -43,4 +44,30 @@ class GenerateModelByLang(luigi.Task):
 		return [GenerateTextByLang(self.lang), GenerateNLPByLang(self.lang)]
 	
 	def run(self):
-		pass
+		d2v = None
+		modelLoc = ""
+		ficheroTweets = None
+		for input in self.input():
+			if "check" in input.path:
+				d2v = Doc2Vec()
+				modelLoc = input.path.replace("check", "model")
+			else:
+				ficheroTweets = input.path
+
+		lab = LabeledLineSentence(ficheroTweets, ides="String")
+		Y = []
+		x = []
+		for tweet in lab:
+			tag = tweet.tags
+			if "POS" in tag[0]:
+				Y.append(1)
+			else:
+				Y.append(0)
+
+			vecX = d2v.simulateVectorsFromVectorText(tweet.words, modelLoc)
+			X.append(vecX)
+
+		Y = np.array(Y)
+		X = np.array(X)
+
+
