109a2f28dd51de464b107815d126b9a21a7094ef
CarlosRosado <carlos.rosado.moral@gmail.com>
2016-04-06 07:21:15 -0400

    Actualización APISentimientos

--- API/APISentimientos.py
+++ API/APISentimientos.py
@@ -1,24 +1,22 @@
 import os
 import luigi
-from LuigiTasks.GenerateSentiment import GenerateSentimentMetions
-#from LuigiTasks.GenerateSim import GenerateSimRelations_semantic, GenerateSimRelations_topics, GenerateSimAll_topics, GenerateSimAll_semantic
+from LuigiTasks.GenerateSentiment import GenerateSentimentMetions, GenerateSentimentUser
 from DBbridge.ConsultasCassandra import ConsultasCassandra
 
 from Config.Conf import Conf
 import json
-
-#import time
 
 import multiprocessing
 
 re_tuser = re.compile(r'@?[a-zA-Z0-9_]+')
 
 
-class _generateSentimentUser(multiprocessing.Process):
-	"""docstring for _generateSentimentUser"""
-	def __init__(self, lang, username, id_tarea):
-		super(_generateSentimentUser, self).__init__()
+class _generateSentiment(multiprocessing.Process):
+	"""docstring for _generateSentiment"""
+	def __init__(self, lang, username, mentions, id_tarea):
+		super(_generateSentiment, self).__init__()
 		self.lang = lang
+		self.mentions = mentions
 		self.username = username
 		self.id_tarea = id_tarea
 
@@ -27,7 +25,10 @@ class _generateSentimentUser(multiprocessing.Process):
 		conf = Conf()
 		path = conf.getAbsPath()
 		comand = "PYTHONPATH=\"${PYTHONPATH}:./LuigiTasks\" && export PYTHONPATH && luigi --module GenerateSentiment "
-		
+		if self.mentions == True:
+			comand += "GenerateSentimentMetions"
+		else:
+			comand += "GenerateSentimentUser"
 		comand += " --lang " + self.lang + "  --idtarea " + str(self.id_tarea)
 		comand += " > /dev/null 2>&1"
 		
@@ -55,6 +56,19 @@ class APISentimientos(object):
 		pos = 0
 		neg = 0
 
+		is_finish = self.isTaskFinished(username, lang,"SentimentsByMentions")
+		
+		if is_finish == False:
+			return False
+		
+		#Lectura del JSON que nos dice los sentimientos de los usuarios
+		with open(sentiment.output().path) as data_file:    
+    		data = json.load(data_file)
+
+    	#Cargamos el numero de positivos y negativos
+    	pos = data["pos"]
+    	neg = data["neg"]
+
 		return pos, neg
 
 	def getSentimentsByUser(username, lang):
@@ -75,22 +89,11 @@ class APISentimientos(object):
 		pos = 0
 		neg = 0
 
-		if len(username) > 16 or len(username) < 2 or re_tuser.match(username) == None:
-			raise Exception("Parametros incorrectos")
-
-		if lang != 'es' and lang != 'en':
-			raise Exception("Parametros incorrectos")
-
-
-		sentiment = GenerateSentimentMetions(lang = lang, username = username)
-		print id_tarea
-
-		#Comprobamos si existe el JSON que nos define a dicho usuario
-		if os.path.isfile(sentiment.output().path) == False:
-			p = _generateSentimentUser(lang, username, id_tarea)
-			p.start()
-			return False
+		is_finish = self.isTaskFinished(username, lang,"SentimentsByUser")
 		
+		if is_finish == False:
+			return False
+
 		#Lectura del JSON que nos dice los sentimientos de los usuarios
 		with open(sentiment.output().path) as data_file:    
     		data = json.load(data_file)
@@ -116,4 +119,34 @@ class APISentimientos(object):
 		-------
 		True si tarea terminada, False si no
 		"""
-		pass
+
+		if len(username) > 16 or len(username) < 2 or re_tuser.match(username) == None:
+			raise Exception("Parametros incorrectos")
+
+		if lang != 'es' and lang != 'en':
+			raise Exception("Parametros incorrectos")
+
+		if tipo_tarea == "SentimentsByMentions"
+			sentiment = GenerateSentimentMetions(lang = lang, username = username)
+			print id_tarea
+
+			#Comprobamos si existe el JSON que nos define a dicho usuario
+			if os.path.isfile(sentiment.output().path) == False:
+				p = _generateSentiment(lang, username, True, id_tarea)
+				p.start()
+				return False
+			else 
+				return True
+		else
+			sentiment = GenerateSentimentUser(lang = lang, username = username)
+			print id_tarea
+
+			#Comprobamos si existe el JSON que nos define a dicho usuario
+			if os.path.isfile(sentiment.output().path) == False:
+				p = _generateSentiment(lang, username, False, id_tarea)
+				p.start()
+				return False
+			else 
+				return True
+
+		
