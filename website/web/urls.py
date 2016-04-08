56a772313f381ecf3b0bc956b78fb744fdcddd1b
garnachod <garnachod@gmail.com>
2016-04-08 11:05:34 +0200

    web funcionando

--- website/web/urls.py
+++ website/web/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^index.html', views.index, name='index'),
     url(r'^dashboard.html', views.dashboard, name='dashboard'),
     url(r'^statistics.html', views.statistics, name='statistics'),
+    url(r'^task.html', views.task, name='task')
 ]
--- website/web/views.py
+++ website/web/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 from django.template import loader
+from django.shortcuts import render, redirect
 from .models import Tarea
+from API.APISentimientos import APISentimientos
 import datetime
 
-
-def getTask(n=10):
-	print Tarea.objects.values_list(flat=True)
 
 def createTask(tipo, username, idioma):
 	t = Tarea(tipo=tipo, username=username, idioma=idioma, inicio=datetime.datetime.now())
@@ -13,23 +13,43 @@ def createTask(tipo, username, idioma):
 
 
 def index(request):
-    template = loader.get_template('mineria/index.html')
-    context = { }
-    #test
-    getTask()
-    return HttpResponse(template.render(context, request))
+	template = loader.get_template('mineria/index.html')
+	context = {}
+
+	return HttpResponse(template.render(context, request))
 
 def dashboard(request):
-    template = loader.get_template('mineria/dashboard.html')
-    context = { }
-    #test
-    getTask()
-    return HttpResponse(template.render(context,request))
+	if 'search' in request.POST and 'lang' in search.POST:
+		createTask('SentimentsByMentions', request.POST['search'], request.POST['lang'])
+		
+	tareas = Tarea.objects.order_by('-inicio')
+	context = {'tareas' : tareas}
+	
+	return render(request, "mineria/dashboard.html", context)
+	#return HttpResponse(tareas)
+
+def task(request):
+	if 'id' not in request.GET:
+		return redirect("dashboard.html")
+
+	idTarea = request.GET['id']
+	tarea = Tarea.objects.get(pk=idTarea)
+
+	retorno = APISentimientos.getSentimentsByMentions(tarea.username.lower(), tarea.idioma.lower())
+	if retorno == False:
+		return redirect("dashboard.html")
+
+	pos, neg = retorno
+	suma = float(pos + neg)
+	pos = float(pos) / suma
+	neg = float(neg) / suma
+	context = {'pos':pos, 'neg':neg}
+	return render(request, "mineria/task.html", context)
+
 
 def statistics(request):
-    template = loader.get_template('mineria/statistics.html')
-    context = { }
-    #test
-    getTask()
-    return HttpResponse(template.render(context,request))
+	template = loader.get_template('mineria/statistics.html')
+	context = { }
+
+	return HttpResponse(template.render(context,request))
 
